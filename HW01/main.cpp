81c3b886766c4416bbcd48f4316cd991cdfd56ac
jeang-bo-yuan <jeangbyj@gmail.com>
2024-03-05 15:27:27 +0800

    Homework01

---
+++ HW01/main.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+	cv::Mat img = cv::imread("Lenna.jpg");
+	assert(img.type() == CV_8UC3);
+
+	for (int row = 0; row < img.rows; ++row) {
+		for (int col = 0; col < img.cols; ++col) {
+			uchar* color = img.ptr(row, col);
+
+			uchar gray = 0.114 * color[0] + 0.587 * color[1] + 0.299 * color[2];
+			color[0] = color[1] = color[2] = gray;
+		}
+	}
+
+
+	cv::imshow("Hello World!", img);
+	cv::waitKey();
+}
